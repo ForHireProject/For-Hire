5409a4d633a2d0c32d1c22b865badfbbeeec347f
Server <november14giel@gmail.com>
2018-01-21 13:36:15 -0500

    added homepage.js and edited api_routes

--- models/post.js
+++ models/post.js
@@ -15,8 +15,8 @@ module.exports = function(sequelize, DataTypes) {
   });
 
   Post.associate = function(models) {
-    // We're saying that a Post should belong to an Author
-    // A Post can't be created without an Author due to the foreign key constraint
+    // We're saying that a Post should belong to an ForHire
+    // A Post can't be created without an ForHire due to the foreign key constraint
     Post.belongsTo(models.ForHire, {
       foreignKey: {
         allowNull: false
