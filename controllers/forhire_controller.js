eaaea67e951db15f46f613f4051a0dd52dde0bbd
Kane Price <price.kane@gmail.com>
2018-01-20 19:43:53 -0500

    added a working directory for us and schema for table creation

---
+++ controllers/forhire_controller.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+var models = require('../models');
+
+router.get('/', function (req, res) {
+});
+
+router.post('/', function (req, res) {
+});
+
+router.put('/', function (req, res) {
+});
+
+router.delete('/', function (req, res) {
+});
+
+module.exports = router;
