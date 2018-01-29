6a603e46bee74eeac886e0416788452d33939327
Server <november14giel@gmail.com>
2018-01-28 23:58:39 -0500

    random image added

--- public/js/category.js
+++ public/js/category.js
@@ -1,10 +1,2 @@
-// PSEUDO CODE:
-//1. Add data id to rendered available service/helper
-//2. When a service is clicked
-//3. Go to page that arranges up a meet-up with service/helper
-//4. Alert the service/helper that they have been selected
 
-//function wrapper 
-$(function () {
-  
-});
+//we probably dont need this file anymore.  
